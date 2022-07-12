ca2e8537107f725bf4be2c31a5dcb8ba4231dbc6
Joaquin Guzman <joaquin.guzman@ucse.net.ar>
2022-07-12 19:46:22 -0300

    con API Eventos,Cursos y Aranceles

--- routes/api.route.js
+++ routes/api.route.js
@@ -1,9 +1,54 @@
 const router = require('express').Router();
-
+const { PrismaClient } = require("@prisma/Client");
+const prisma = new PrismaClient();
 
 
 router.get('/', async (req, res, next) => {
   res.send({ message: 'Ok api is working 🚀' });
 });
+
+
+router.get("/grupocurso",async(req,res,next)=>{
+try{
+const eventos = await prisma.GrupoCurso.findMany({});
+res.json(eventos);
+
+}catch(error){next(error)}
+
+})
+
+router.get("/cursosxevento/:id_evento",async(req,res,next)=>{
+const {id_evento} = req.params;
+  try {
+  const cursos = await prisma.Cursos.findMany({
+where:{
+  idGrupoCurso : parseInt(id_evento)
+}
+  });
+  res.json(cursos);
+  
+} catch (error) {
+  next(error)
+}
+})
+
+router.get ("/arancelesxcurso/:id_curso",async (req,res,next)=>{
+const {id_curso} = req.params;
+try {
+    const aranceles = await prisma.Aranceles.findMany({
+  where:{
+    IdCurso : parseInt(id_curso)  
+        }
+  })
+  res.json(aranceles);
+  
+} catch (error) {
+  next(error)
+}
+
+
+})
+
+
 
 module.exports = router;
