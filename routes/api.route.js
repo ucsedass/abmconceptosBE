8ab78d154876a6551c886c686c264a16b9761ecf
Joaquin Guzman <joaquin.guzman@ucse.net.ar>
2022-08-04 20:39:39 -0300

    jueves 4

--- routes/api.route.js
+++ routes/api.route.js
@@ -59,6 +59,19 @@ router.get("/cursoxid/:id_curso", async (req, res, next) => {
   }
 });
 
+router.get("/buscarevento/:codigo", async (req, res, next) => {
+  const { codigo } = req.params;
+  try {
+    const eventos = await prisma.GrupoCurso.findMany({
+      where: {
+        codigo: codigo,
+      },
+    });
+    res.json(eventos);
+  } catch (error) {
+    next(error);
+  }
+});
 router.post("/nuevoarancel", async (req, res, next) => {
   const { IdCurso, FechaDesde, FechaHasta, Precio, CantidadUnidadesMinima } =
     req.body;
@@ -103,57 +116,6 @@ router.post("/nuevocurso", async (req, res, next) => {
   } = req.body;
 
   try {
-    const nuevoCurso = await prisma.Cursos.create({
-      data: {
-        nombre1,
-        nombre2,
-        codigo,
-        idGrupoCurso,
-        fechaInicio,
-        fechaFin,
-        cupoMaximo,
-        habilitado,
-        resaltar,
-        codUnidadAcademica,
-        idPrograma,
-        idPlanEstudio,
-        idObligacion,
-        mailReferencia,
-        domicilioReferencia,
-        NombreContactoReferencia,
-        urlUbicacion,
-        RequiereValidacionEmail,
-      },
-    });
-    res.send(nuevoCurso);
-  } catch (error) {
-    next("Este es el error", error);
-  }
-});
-
-router.post("/nuevocurso2", async (req, res, next) => {
-  const {
-    nombre1,
-    nombre2,
-    codigo,
-    idGrupoCurso,
-    fechaInicio,
-    fechaFin,
-    cupoMaximo,
-    habilitado,
-    resaltar,
-    codUnidadAcademica,
-    idPrograma,
-    idPlanEstudio,
-    idObligacion,
-    mailReferencia,
-    domicilioReferencia,
-    NombreContactoReferencia,
-    urlUbicacion,
-    RequiereValidacionEmail,
-  } = req.body;
-
-  try {
     const cc = await prisma.Cursos.create({
       data: {
         nombre1,
@@ -176,6 +138,26 @@ router.post("/nuevocurso2", async (req, res, next) => {
         RequiereValidacionEmail,
       },
     });
+    res.json(cc);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/nuevoevento", async (req, res, next) => {
+  const { codigo, Nombre, descripcion, habilitado, RequiereValidacionEmail } =
+    req.body;
+  try {
+    const nuevoevento = await prisma.grupoCurso.create({
+      data: {
+        codigo,
+        Nombre,
+        descripcion,
+        habilitado,
+        RequiereValidacionEmail,
+      },
+    });
+    res.json(nuevoevento);
   } catch (error) {
     next(error);
   }
